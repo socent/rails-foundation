880de7e9a8088f6b165fa52f862f997b3e72aff2
Brad Wilcox <brad_wilcox@me.com>
2014-04-13 23:35:01 -0400

    set up interchange for media querie responsive images; initializing js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
+//= require foundation.interchange
+//= require reveal
 //= require_tree .
 $(function() {
   $(document).foundation();
